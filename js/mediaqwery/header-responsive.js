d4837c7715cf6584af57b11cd17f6c9537022171
Leandro Bellone <Belloneleandro@gmail.com>
2024-10-07 02:28:58 -0300

    Tercer Commit

--- js/mediaqwery/header-responsive.js
+++ js/mediaqwery/header-responsive.js
@@ -1,15 +1,18 @@
 
     const BotClose = document.getElementById('boton-cerrar-hamburguesa');
     const BotOpen = document.getElementById('menu-responsive');
+    const ImgHeader = document.getElementById('img-header');
     const ResponsiveHeaderContent = document.getElementById('responsive-header-content');
 
         BotClose.addEventListener('click', function() {
             ResponsiveHeaderContent.classList.remove('appear');
             BotOpen.classList.remove('disappear');
+            ImgHeader.classList.remove('disappear');
         });
 
         BotOpen.addEventListener('click', function() {
             ResponsiveHeaderContent.classList.add('appear');
             BotOpen.classList.add('disappear');
+            ImgHeader.classList.add('disappear');
         });
 
